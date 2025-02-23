672483ad17b5d912c19e58a73ebac1fabb6b1bbd
ND15 <nikhildas162000@gmail.com>
2025-02-23 11:07:14 +0530

    [MODIFICATION] - number of layers

--- diffwave/utilities/params.py
+++ diffwave/utilities/params.py
@@ -9,21 +9,21 @@ class Params:
     batch_size: int = 4
     learning_rate: float = 2e-4
     max_grad_norm: Optional[float] = None
-    epochs: int = 10
-    time_steps: int = 50
+    epochs: int = 100
+    time_steps: int = 1000
 
-    sample_rate: int = 22050
+    sample_rate: int = 16000
     n_mels: int = 80
     n_fft: int = 1024
     hop_samples: int = 256
     crop_mel_frames: int = 62
 
     # Model params
-    residual_layers: int = 10
+    residual_layers: int = 20
     residual_channels: int = 64
     dilation_cycle_length: int = 10
     condition: bool = False
-    noise_schedule: List[float] = field(default_factory=lambda: cosine_schedule(time_steps=50))
+    noise_schedule: List[float] = field(default_factory=lambda: cosine_schedule(time_steps=1000))
     inference_noise_schedule: List[float] = field(default_factory=lambda: [0.0001, 0.001, 0.01, 0.05, 0.2, 0.5])
 
-    audio_len: int = 16000 * 3
+    audio_len: int = 16000 * 1
