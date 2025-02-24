4146a0b11b1dd6dc905a6c51aad16d0e3bf7b4e1
ND15 <nikhildas162000@gmail.com>
2025-02-24 09:02:49 +0530

    [MODIFICATION] - added saving and loading checkpoints

--- diffwave/trainer.py
+++ diffwave/trainer.py
@@ -1,14 +1,12 @@
 import os
-import sys
 
-sys.path.append(os.getcwd())
-print(sys.path)
 import soundfile
 import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+
 from model import DiffWave
 from sampler.ddpm import DDPMSampler
 from utilities.dataset import UnconditionalAudioDataset
@@ -16,12 +14,7 @@ from utilities.params import Params
 
 
 class DiffWaveTrainer:
-    def __init__(self, model: DiffWave,
-                 sampler,
-                 optimizer,
-                 dataset,
-                 model_dir,
-                 params):
+    def __init__(self, model: DiffWave, sampler, optimizer, dataset, model_dir, params):
         if not os.path.exists(model_dir):
             os.makedirs(name=model_dir, exist_ok=True)
 
@@ -33,22 +26,16 @@ class DiffWaveTrainer:
         self.sampler = sampler
 
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-
-        # TODO tune this for mixed precision training
         self.scaler = torch.cuda.amp.GradScaler()
         self.step = 0
         betas = torch.tensor(self.params.noise_schedule)
         alphas = 1.0 - betas
         self.alphas = alphas.to(self.device)
-        self.alphas_cum_prod = torch.cumprod(alphas, dim=-1)
-        self.alphas_cum_prod = self.alphas_cum_prod.to(self.device)
+        self.alphas_cum_prod = torch.cumprod(alphas, dim=-1).to(self.device)
         self.sqrt_alphas_cum_prod = torch.sqrt(self.alphas_cum_prod).to(self.device)
         self.sqrt_one_minus_alphas_cum_prod = torch.sqrt(1. - self.alphas_cum_prod).to(self.device)
         self.loss_fn = nn.MSELoss()
         self.epochs = params.epochs
-
-    def loss_function(self):
-        pass
 
     def train_step(self, audio: Tensor, condition=None):
         audio = audio.to(device=self.device)
@@ -56,7 +43,6 @@ class DiffWaveTrainer:
             condition = condition.to(self.device)
 
         N, T = audio.shape
-
         self.optimizer.zero_grad()
 
         t = torch.randint(low=0, high=len(self.params.noise_schedule), size=[N], device=self.device)
@@ -70,16 +56,20 @@ class DiffWaveTrainer:
             loss = self.loss_fn(noise, predicted.squeeze(1))
 
         self.scaler.scale(loss).backward()
-
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
-
         self.scaler.step(self.optimizer)
         self.scaler.update()
 
         return loss
 
-    def train(self):
-        for epoch in tqdm(range(self.epochs), desc="Epochs"):
+    def train(self, resume_checkpoint: str = f"train/checkpoint_epoch_{16}.pt"):
+        start_epoch = 0
+        if resume_checkpoint:
+            start_epoch = self.load_checkpoint(resume_checkpoint)
+
+        # printc(f"[INFO] Dataset Length: {len(self.dataset)}")
+
+        for epoch in tqdm(range(start_epoch, self.epochs), desc="Epochs"):
             epoch_loss = 0
             for i, data in tqdm(enumerate(self.dataset), desc=f"Epoch {epoch + 1}"):
                 if len(data) == 1:
@@ -90,37 +80,62 @@ class DiffWaveTrainer:
 
                 loss = self.train_step(audio=audio, condition=condition)
                 epoch_loss += loss.detach().cpu()
-                if (i + 1) % 200 == 0:
-                    print(f"Epoch {epoch + 1:03d} | Loss: {epoch_loss / i:.4f}")
-                    # samples = self.sampler.sampling(n_samples=10)
-                    # for j, sample in enumerate(samples):
-                    #     if not os.path.exists(self.model_dir + f"/epoch_{epoch + 1}_iter_{i}"):
-                    #         os.makedirs(self.model_dir + f"/epoch_{epoch + 1}_iter_{i}")
-                    #
-                    #     soundfile.write(f"{self.model_dir}/epoch_{epoch + 1}_iter_{i}/sample_{j}.wav",
-                    #                     data=sample.detach().cpu().numpy(),
-                    #                     samplerate=16000)
 
             avg_loss = epoch_loss / len(self.dataset)
-            print(f"Epoch {epoch + 1:03d} | Loss: {avg_loss:.4f}")
-            if (epoch + 1) % 30 == 0:
-                samples = self.sampler.sampling(n_samples=10)
-                for j, sample in enumerate(samples):
-                    if not os.path.exists(self.model_dir + f"/epoch_{epoch + 1}"):
-                        os.makedirs(self.model_dir + f"/epoch_{epoch + 1}")
+            print(f"Epoch {epoch + 1:03d} | Avg Loss: {avg_loss:.4f}")
 
-                    soundfile.write(f"{self.model_dir}/epoch_{epoch + 1}/sample_{j}.wav",
-                                    data=sample.detach().cpu().numpy(),
-                                    samplerate=16000)
+            # Save checkpoint every 2 epochs
+            if (epoch + 1) % 2 == 0:
+                self.save_checkpoint(epoch + 1)
+                # samples = self.sampler.sampling(n_samples=10)
+                # for j, sample in enumerate(samples):
+                #     if not os.path.exists(self.model_dir + f"/epoch_{epoch + 1}"):
+                #         os.makedirs(self.model_dir + f"/epoch_{epoch + 1}")
+                #
+                #     soundfile.write(f"{self.model_dir}/epoch_{epoch + 1}/sample_{j}.wav",
+                #                     data=sample.detach().cpu().numpy(),
+                #                     samplerate=16000)
+
+    def generate(self, model_path: str = f"train/checkpoint_epoch_{16}.pt", num_samples: int = 10):
+        self.load_checkpoint(model_path)
+        samples = self.sampler.sampling(n_samples=num_samples)
+        for j, sample in enumerate(samples):
+            if not os.path.exists(self.model_dir + f"/generated"):
+                os.makedirs(self.model_dir + f"/generated")
+            soundfile.write(f"{self.model_dir}/generated/sample_{j}.wav",
+                            data=sample.detach().cpu().numpy(),
+                            samplerate=16000)
+
+    def save_checkpoint(self, epoch: int):
+        checkpoint = {
+            'epoch': epoch,
+            'step': self.step,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'scaler_state_dict': self.scaler.state_dict(),
+        }
+        checkpoint_path = os.path.join(self.model_dir, f"checkpoint_epoch_{epoch}.pt")
+        torch.save(checkpoint, checkpoint_path)
+        print(f"Checkpoint saved at {checkpoint_path}")
+
+    def load_checkpoint(self, checkpoint_path: str):
+        checkpoint = torch.load(checkpoint_path, map_location=self.device)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.scaler.load_state_dict(checkpoint['scaler_state_dict'])
+        self.step = checkpoint['step']
+        epoch = checkpoint['epoch']
+        print(f"Checkpoint loaded from {checkpoint_path}. Resuming from epoch {epoch}, step {self.step}")
+        return epoch
 
 
 if __name__ == "__main__":
     params = Params()
-    audio_dataset = UnconditionalAudioDataset("../../../../../Data/audio/audio/", num_segments=3)
-    dataloader = DataLoader(audio_dataset, batch_size=8, shuffle=True)
+    audio_dataset = UnconditionalAudioDataset("../../../../../Data/audio/audio/", num_segments=2, segment_length=3)
+    dataloader = DataLoader(audio_dataset, batch_size=16, shuffle=True)
     model = DiffWave(params=params).to(device="cuda")
     sampler = DDPMSampler(model=model, params=params)
-    optimizer = torch.optim.Adam(model.parameters(), lr=2e-4)
+    optimizer = torch.optim.Adam(model.parameters())
     trainer = DiffWaveTrainer(model=model,
                               sampler=sampler,
                               dataset=dataloader,
@@ -128,3 +143,4 @@ if __name__ == "__main__":
                               model_dir="train",
                               params=params)
     trainer.train()
+    # trainer.generate()
--- diffwave/utilities/params.py
+++ diffwave/utilities/params.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import List, Optional
 
-from utilities.utils import cosine_schedule
+import numpy as np
 
 
 @dataclass
@@ -10,7 +10,7 @@ class Params:
     learning_rate: float = 2e-4
     max_grad_norm: Optional[float] = None
     epochs: int = 100
-    time_steps: int = 1000
+    time_steps: int = 50
 
     sample_rate: int = 16000
     n_mels: int = 80
@@ -19,11 +19,11 @@ class Params:
     crop_mel_frames: int = 62
 
     # Model params
-    residual_layers: int = 20
+    residual_layers: int = 30
     residual_channels: int = 64
     dilation_cycle_length: int = 10
     condition: bool = False
-    noise_schedule: List[float] = field(default_factory=lambda: cosine_schedule(time_steps=1000))
+    # noise_schedule: List[float] = field(default_factory=lambda: cosine_schedule(time_steps=1000))
+    noise_schedule: List[float] = field(default_factory=lambda: np.linspace(1e-4, 0.05, 50).tolist())
     inference_noise_schedule: List[float] = field(default_factory=lambda: [0.0001, 0.001, 0.01, 0.05, 0.2, 0.5])
-
-    audio_len: int = 16000 * 1
+    audio_len: int = 16000 * 3
